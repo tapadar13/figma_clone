be3cf68446f22b938af31dfa02b58741567ffcdf
tapadar13 <monsurismyname@gmail.com>
2024-02-12 23:08:04 +0530

    chore: Add assets and configure tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
   return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
+    <div className="h-[100vh] w-full flex justify-center items-center text-center">
+      <h1 className="font-2xl text-white">Liveblocks Figma Clone</h1>
+    </div>
   );
 }
