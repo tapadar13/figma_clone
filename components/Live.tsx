b020aa3dba5a3684ed9ac7887831d85efe3ff72d
tapadar13 <monsurismyname@gmail.com>
2024-02-17 19:15:46 +0530

    feat: integrate live chat alongside live cursor
    Extend the existing Liveblocks integration to include real-time live chat functionality. Users can now communicate in real-time, enhancing collaborative interactions within the app. This builds upon the previously implemented live cursor feature, providing a seamless and interactive user experience.

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,10 +1,17 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import LiveCursors from "./cursor/LiveCursors";
 import { useMyPresence, useOthers } from "@/liveblocks.config";
+import { CursorMode, CursorState } from "@/types/type";
+import CursorChat from "./cursor/CursorChat";
 
 const Live = () => {
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  // track the state of the cursor (hidden, chat, reaction, reaction selector)
+  const [cursorState, setCursorState] = useState<CursorState>({
+    mode: CursorMode.Hidden,
+  });
 
   // Listen to mouse events to change the cursor state
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
@@ -25,7 +32,9 @@ const Live = () => {
 
   // Hide the cursor when the mouse leaves the canvas
   const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-    event.preventDefault();
+    setCursorState({
+      mode: CursorMode.Hidden,
+    });
 
     updateMyPresence({
       cursor: null,
@@ -49,13 +58,22 @@ const Live = () => {
 
   return (
     <div
-      className="relative flex h-full w-full flex-1 items-center justify-center border-2 border-green-500"
+      className="flex h-[100vh] w-full justify-center items-center text-center border-2 border-green-500"
       id="canvas"
       onPointerMove={handlePointerMove}
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
     >
       <h1 className="font-2xl text-white">Liveblocks Figma Clone</h1>
+
+      {cursor && (
+        <CursorChat
+          cursor={cursor}
+          cursorState={cursorState}
+          setCursorState={setCursorState}
+          updateMyPresence={updateMyPresence}
+        />
+      )}
       <LiveCursors others={others} />
     </div>
   );
