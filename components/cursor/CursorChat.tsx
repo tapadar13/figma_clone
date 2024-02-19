1806fa01dd5ba088b405bf17210e2d8fb526e588
tapadar13 <monsurismyname@gmail.com>
2024-02-19 11:12:19 +0530

    fix: Prevent reaction bar from appearing when chat input is open
    Resolve an issue where pressing the "e" key would incorrectly trigger the reaction bar to appear even when the chat input is open. Implement a fix to ensure that the reaction bar is only activated when the chat input is closed. This adjustment maintains a consistent user experience and resolves the unintended behavior.

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -44,7 +44,10 @@ const CursorChat = ({
         <>
           <CursorSVG color="#000" />
 
-          <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
+          <div
+            className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]"
+            onKeyUp={(e) => e.stopPropagation()}
+          >
             {/* If there is a previous message, show it above the input */}
             {cursorState.previousMessage && (
               <div>{cursorState.previousMessage}</div>
